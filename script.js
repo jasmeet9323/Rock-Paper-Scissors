ef9d903c1da4a4f1beb0ed4a6f1abc1279e7cd04
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-30 14:48:28 -0700

    Move the script at the end so as to load DOM content before execution of the script.

--- script.js
+++ script.js
@@ -111,3 +111,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log(document.getElementById("paper").innerHTML);
 });
+
+
+let rockButton = document.getElementById("rock");
+rockButton.addEventListener('click', (e) => {
+    console.log(e);
+})
