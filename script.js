2a0d9043ab541cd35dce4a78afa6fbaf2e63ba34
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2020-05-12 09:57:07 -0700

    Rewrite the app to make it streamlined.

--- script.js
+++ script.js
@@ -1,112 +1,127 @@
-var nGames = 0;
-var computerScore = 0;
-var playerScore = 0;
-var ties = 0;
+$(document).ready(function () {
+  var nGames = 0;
+  var scores = { computerScore: 0, playerScore: 0, ties: 0 };
 
-function computerPlay() {
-	let number = Math.floor(Math.random() * 3 + 1);
-	if (number == 1)
-		return "Rock";
-	else if(number == 2)
-		return "Paper";
-	else return "Scissors";
-}
-function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase();
-	computerSelection = computerSelection.toLowerCase();
-	let winner = 0;
-	switch(playerSelection){
-		case "rock": {
-			if(computerSelection=="scissors")
-				winner = 1;
-			else if(computerSelection=="paper")
-				winner = 2;
-			else
-				winner = 0;
-			break;
-		}
-		case "paper": {
-			if(computerSelection=="rock")
-				winner = 1;
-			else if (computerSelection=="scissors")
-				winner = 2;
-			else
-				winner = 0;
-			break;
-		}
-		case "scissors":{
-			if(computerSelection == "paper")
-				winner = 1;
-			else if(computerSelection == "rock")
-				winner = 2;
-			else
-				winner = 0;
-			break;
-		}
-	}
-	nGames++;
-	console.log(nGames);
+  function computerPlay() {
+    let number = Math.floor(Math.random() * 3 + 1);
+    if (number == 1) return "rock";
+    else if (number == 2) return "paper";
+    else return "scissors";
+  }
 
-	var ans;
-	if (winner == 1){
-		playerScore++;
-		ans = "You Win! <br /> Your score:"+ playerScore +"<br /> Computer Score:"+computerScore +"<br /> Ties:"+ties;
-	}
-	else if(winner == 2){
-		computerScore++;
-		ans = "You Lose! <br /> Your score:"+ playerScore +"<br /> Computer Score:"+computerScore +"<br /> Ties:"+ties;
-	}
-	else {
-		ties++;
-		ans = "Tie!\n <br /> Your score:"+ playerScore +"<br /> Computer Score:"+computerScore +"<br />Ties:"+ties;
-	}
+  function compileMessage(winner, scores) {
+    var message;
+    if (winner == 1) {
+      scores.playerScore++;
+      message =
+        "You Win! <br /><br /> Your score: " +
+        scores.playerScore +
+        "<br /> Computer Score: " +
+        scores.computerScore +
+        "<br /> Ties: " +
+        scores.ties;
+    } else if (winner == 2) {
+      scores.computerScore++;
+      message =
+        "You Lose! <br /><br /> Your score: " +
+        scores.playerScore +
+        "<br /> Computer Score: " +
+        scores.computerScore +
+        "<br /> Ties: " +
+        scores.ties;
+    } else {
+      scores.ties++;
+      message =
+        "Tie!\n <br /><br /> Your score: " +
+        scores.playerScore +
+        "<br /> Computer Score: " +
+        scores.computerScore +
+        "<br />Ties: " +
+        scores.ties;
+    }
+    return message;
+  }
 
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let winner = 0;
+    switch (playerSelection) {
+      case "rock": {
+        if (computerSelection == "scissors") winner = 1;
+        else if (computerSelection == "paper") winner = 2;
+        else winner = 0;
+        break;
+      }
+      case "paper": {
+        if (computerSelection == "rock") winner = 1;
+        else if (computerSelection == "scissors") winner = 2;
+        else winner = 0;
+        break;
+      }
+      case "scissors": {
+        if (computerSelection == "paper") winner = 1;
+        else if (computerSelection == "rock") winner = 2;
+        else winner = 0;
+        break;
+      }
+    }
+    nGames++;
+    //console.log(nGames);
 
-	if(nGames == 5){
-		console.log(document.getElementById("demo").innerHTML = "");
+    //console.log(winner, scores.playerScore, scores.computerScore, scores.ties);
+    var ans = compileMessage(winner, scores);
+    //console.log(scores.playerScore, scores.computerScore, scores.ties);
 
-		if(playerScore > computerScore){
-			console.log(document.getElementById("demo").innerHTML = "Congratulations! You Win.");
-		} else if(playerScore < computerScore){
-			console.log(document.getElementById("demo").innerHTML = "Sorry! You Lose.");
-		} else {
-			console.log(document.getElementById("demo").innerHTML = "Alas! It's a tie");
-		}
-		setTimeout(function(){document.getElementById("demo").innerHTML = "Press Ctrl+Shift+I to see final result. <br /> <br /> Starting new game.";},1000);
-		
-		computerScore =0;
-		playerScore = 0;
-		ties = 0;
-		nGames = 0;
-	}
-	return ans;
-}
+    if (nGames == 5) {
+      var result;
+      if (scores.playerScore > scores.computerScore) {
+        result = "Congratulations! You Win.";
+      } else if (scores.playerScore < scores.computerScore) {
+        result = "Sorry! You Lose.";
+      } else {
+        result = "Alas! It's a tie";
+      }
+	  document.querySelector("#result").innerHTML = result;
+      setTimeout(function () {
+        document.querySelector("#result").innerHTML = "";
+      }, 1000);
 
-function selectRock(pSelect) {
-	var pSelect = document.getElementById("rock").value;
-	var cSelect = computerPlay();
-	var result = playRound( pSelect, cSelect);
-	document.getElementById("demo").innerHTML = result;
-}
-function selectPaper(pSelect) {
-	var pSelect = document.getElementById("paper").value;
-	var cSelect = computerPlay();
-	var result = playRound( pSelect, cSelect);
-	document.getElementById("demo").innerHTML = result;
-}
-function selectScissors(pSelect) {
-	var pSelect = document.getElementById("scissors").value;
-	var cSelect = computerPlay();
-	var result = playRound( pSelect, cSelect);
-	document.getElementById("demo").innerHTML = result;
-}
+      scores.computerScore = 0;
+      scores.playerScore = 0;
+      scores.ties = 0;
+      nGames = 0;
+    }
+    return ans;
+  }
 
+  function selectRock(pSelect) {
+    var pSelect = document.getElementById("rock").value;
+    var cSelect = computerPlay();
+    var result = playRound(pSelect, cSelect);
+    document.getElementById("ranking").innerHTML = result;
+  }
 
+  function selectPaper(pSelect) {
+    var pSelect = document.getElementById("paper").value;
+    var cSelect = computerPlay();
+    var result = playRound(pSelect, cSelect);
+    document.getElementById("ranking").innerHTML = result;
+  }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-	console.log(document.getElementById("rock").innerHTML);
-});
+  function selectScissors(pSelect) {
+    var pSelect = document.getElementById("scissors").value;
+    var cSelect = computerPlay();
+    var result = playRound(pSelect, cSelect);
+    document.getElementById("ranking").innerHTML = result;
+  }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-	console.log(document.getElementById("paper").innerHTML);
+  const rockButton = document.querySelector("#rock");
+  rockButton.addEventListener("click", selectRock);
+
+  const paperButton = document.querySelector("#paper");
+  paperButton.addEventListener("click", selectPaper);
+
+  const scissorsButton = document.querySelector("#scissors");
+  scissorsButton.addEventListener("click", selectScissors);
 });
